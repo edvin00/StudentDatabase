b2856cd89589b2812c0b4ac9c5e73e02799c9ba2
Edvin <edvin.fazlic@outlook.com>
2021-10-03 17:43:23 +0200

    yd

--- StudentDatabase/StudentDatabase.cpp
+++ StudentDatabase/StudentDatabase.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
 #include <stdint.h>
 #include <string>
+#include <vector>
+#include <fstream>
 
 class StudentDatabase {
 
 private:
-       
-    const std::string menuItems[4] = {"Add student", "Query student", "Remove student", "Quit"};
+
+    struct SubjectEntry;
+    struct StudentEntry;
+
+    const std::string startMenuItems[4] = { "Add student", "Query student", "Remove student", "Quit" };
 
     struct StudentEntry {
-        std::string name, surname;
+        std::string name, surname, indexNumber;
+        std::vector<SubjectEntry> passedSubjectsVector;
+        StudentEntry(std::string name, std::string surname, std::string indexNumber) : name{ name }, surname{ surname }, indexNumber{ indexNumber } {};
     };
 
+    struct SubjectEntry {
+        std::string subjectCode, subjectName;
+        std::vector<SubjectEntry> requiredSubjectsVector;
+    };    
+
+    std::vector<StudentEntry> studentEntryVector;
+       
     unsigned int numberOfStudents = 0;
       
-    //TODO
     void addStudent() {
-    
-        auto newStudent = StudentEntry();
+        std::string name, surname, indexNumber;
 
+        std::cout << "Name Surname IndexNumber: ";
+        std::cin >> name >> surname >> indexNumber;
+
+        auto newStudent = StudentEntry(name, surname, indexNumber);
+        studentEntryVector.push_back(newStudent);
+        numberOfStudents++;
     }
 
     //TODO
@@ -35,6 +53,20 @@ public:
 
     StudentDatabase() {
         
+        //load subject list from file
+
+        std::string line;
+        std::ifstream subjectsFile;
+        subjectsFile.open("listOfSubjects.txt");
+        if (!subjectsFile.is_open()) {
+            std::cout << "Error reading subjects file! Program aborted." << std::endl;
+            return;
+        }
+        while (std::getline(subjectsFile, line)) {
+
+        }
+        subjectsFile.close();
+
     }
 
     void displayMenu() {
@@ -42,7 +74,7 @@ public:
         counter = 1;
 
         std::cout << "Menu:" << std::endl;
-        for (auto item : menuItems) {
+        for (auto item : startMenuItems) {
             std::cout << "\t" << counter++ << ". " << item << std::endl;
         }
         std::cout << std::endl << "Choice: ";
